6d284a526f5a5f62fa4f2b4394f7ff4c8776efe2
Bartosz Rokita <bartek@do-usterki.pl>
2019-04-01 21:26:18 +0200

    init

---
+++ GrafikaProj/ImageCustomizator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GrafikaProj
+{
+    class ImageCustomizator
+    {
+        BitmapSource sourceBitmap;
+        BitmapSource customizedBitmap;
+        public ImageCustomizator()
+        {
+
+        }
+
+        public void SetSource(BitmapSource source)
+        {
+            BitmapSource grayImage = new FormatConvertedBitmap(source, PixelFormats.Gray8, null, 0);
+            this.sourceBitmap = grayImage;
+            this.customizedBitmap = grayImage;
+        }
+
+        public BitmapSource GetCustomizedSource()
+        {
+            return customizedBitmap;
+        }
+
+    }
+}
--- GrafikaProj/MainWindow.xaml.cs
+++ GrafikaProj/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace GrafikaProj
 {
 	/// <summary>
@@ -19,10 +20,26 @@ namespace GrafikaProj
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-
-		public MainWindow()
+        ImageCustomizator imageCustomizator;
+        public MainWindow()
 		{
+            imageCustomizator = new ImageCustomizator();
 			InitializeComponent();
 		}
-	}
+
+        private void LoaderClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Załaduj obrazek";
+            dialog.Filter = "Pliki obrazów (*.bmp)|*.bmp";
+            if (dialog.ShowDialog() == true)
+            {
+                BitmapImage loadedImage = new BitmapImage(new Uri(dialog.FileName));
+                this.imageCustomizator.SetSource(loadedImage);
+                OriginImageViewer.Source = this.imageCustomizator.GetCustomizedSource();
+                CustomizedImageViewer.Source = this.imageCustomizator.GetCustomizedSource();
+
+            }
+        }
+    }
 }
